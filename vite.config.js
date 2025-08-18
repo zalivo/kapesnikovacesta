a10820987afdc894f6013d710ee18b0d0cbd66ed
Ivo <izaloudek5@gmail.com>
2025-08-18 12:33:13 +0200

    does this fix it?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+    base: "./",
+    plugins: [tailwindcss()],
+});
